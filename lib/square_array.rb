fd547f63cea3e0857db71370ff20106659a152fc
Anthony Alcorn <xmastony@gmail.com>
2020-08-14 20:16:43 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,21 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  #I NEEED TO COME REVIEW THIS CONCEPT
+  # Allocate an array with the same size as `numbers`
+  # so that the runtime does not have to resize it from time to time
+  result = Array.new(numbers.size)
+
+  # The index
+  i = 0
+
+  while i < numbers.size
+    # Fill the result array
+    result[i] = numbers[i] ** 2
+
+    # and don't forget to increase the index,
+    # otherwise the loop will run forever.
+    i += 1
+  end
+
+  # Return the result array
+  result
 end
